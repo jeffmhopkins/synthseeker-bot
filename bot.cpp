d377683ba024057dae2ef386f6be3fa1f3ee8924
jeffmhopkins <jeff.m.hopkins@gmail.com>
2024-05-24 22:33:34 -0400

    Update bot.cpp

--- bot.cpp
+++ bot.cpp
@@ -1,95 +1,92 @@
 #include <dpp/dpp.h>
-#include <iomanip>
-#include <sstream>
- 
+
+//version v1.2
+
+long discord_channel = 0123456789012345678; //channelID for announcements NEEDS UPDATED
+std::string discord_notification_role = "<@&0123456789012345678>"; //Role to tag NEEDS UPDATED
+std::string discord_token = "TOKEN"; //Bot Token NEEDS UPDATED
+
+//Internal Temporary Server Variables
+bool server_needs_announcement = false;
+bool allow_announcements = true;
+std::string ninjam_user_list = "";
+std::string ninjam_user_list_get = "";
+int get_status = 0;
+
 int main() {
-    /* Setup the bot */
-    dpp::cluster bot("token");
- 
+    dpp::cluster bot(discord_token);
+
     bot.on_log(dpp::utility::cout_logger());
- 
-    /* The event is fired when someone issues your commands */
-    bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
- 
-        /* Check which command they ran */
-        if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
+
+    bot.on_slashcommand([&bot](const dpp::slashcommand_t & event) {
+        if (event.command.get_command_name() == "info") {
+			event.reply(dpp::message("The Synthseeker NINJAM Server address is ninjam.synthseeker.onlin:2049").set_flags(dpp::m_ephemeral));
         }
-
-        if (event.command.get_command_name() == "join") {
- 
-            /* Get the guild */
-            dpp::guild* g = dpp::find_guild(event.command.guild_id);
- 
-            /* Get the voice channel that the bot is currently in from this server (will return nullptr if we're not in a voice channel!) */
-            auto current_vc = event.from->get_voice(event.command.guild_id);
- 
-            bool join_vc = true;
- 
-            /* Are we in a voice channel? If so, let's see if we're in the right channel. */
-            if (current_vc) {
-                /* Find the channel id that the user is currently in */
-                auto users_vc = g->voice_members.find(event.command.get_issuing_user().id);
-                
-                if (users_vc != g->voice_members.end() && current_vc->channel_id == users_vc->second.channel_id) {
-                    join_vc = false;
-                    
-                    /* We are on this voice channel, at this point we can send any audio instantly to vc:
- 
-                     * current_vc->send_audio_raw(...)
-                     */
-                } else {
-                    /* We are on a different voice channel. We should leave it, then join the new one 
-                     * by falling through to the join_vc branch below.
-                     */
-                    event.from->disconnect_voice(event.command.guild_id);
- 
-                    join_vc = true;
-                }
-            }
- 
-            /* If we need to join a vc at all, join it here if join_vc == true */
-            if (join_vc) {
-                /* Attempt to connect to a voice channel, returns false if we fail to connect. */
- 
-                /* The user issuing the command is not on any voice channel, we can't do anything */
-                if (!g->connect_member_voice(event.command.get_issuing_user().id)) {
-                    event.reply("You don't seem to be in a voice channel!");
-                    return;
-                }
- 
-                /* We are now connecting to a vc. Wait for on_voice_ready 
-                 * event, and then send the audio within that event:
-                 * 
-                 * event.voice_client->send_audio_raw(...);
-                 * 
-                 * NOTE: We can't instantly send audio, as we have to wait for
-                 * the connection to the voice server to be established!
-                 */
- 
-                /* Tell the user we joined their channel. */
-                event.reply("Joined your channel!");
-            } else {
-                event.reply("Don't need to join your channel as i'm already there with you!");
-            }
+		if (event.command.get_command_name() == "settings") {
+            std::string announcement_enable = std::get<std::string>(event.get_parameter("announcements"));
+			if(announcement_enable == "announce_enable") {
+				event.reply(dpp::message("Channel announcements are now enabled.").set_flags(dpp::m_ephemeral));
+				allow_announcements = true;
+			}
+			if(announcement_enable == "announce_disable") {
+				event.reply(dpp::message("Channel announcements are now disabled.").set_flags(dpp::m_ephemeral));
+				allow_announcements = false;
+			}
         }
-
-
     });
  
-    bot.on_ready([&bot](const dpp::ready_t & event) {
-        if (dpp::run_once<struct register_bot_commands>()) {
-            /* Create a new command. */
-            bot.global_command_create(dpp::slashcommand("join", "Joins your voice channel.", bot.me.id));
-        }
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+	//if (dpp::run_once<struct clear_bot_commands>()) {
+        //    bot.global_bulk_command_delete();
+        //}
 		
-		if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(dpp::slashcommand("info", "Information about the NINJAM Server", bot.me.id));
+			dpp::slashcommand settingscommand("settings", "change settings of the NINJAM bot.", bot.me.id);
+            settingscommand.add_option(
+                dpp::command_option(dpp::co_string, "announcements", "Allow server change messages in channel", true)
+                    .add_choice(dpp::command_option_choice("enable", std::string("announce_enable")))
+                    .add_choice(dpp::command_option_choice("disable", std::string("announce_disable")))
+            );
+ 
+            bot.global_command_create(settingscommand);
         }
+
+        bot.start_timer([&bot](const dpp::timer& timer){
+			bot.request(
+				"http://ninjam.synthseeker.online/util/discord.php?a=users", dpp::m_get, [&bot](const dpp::http_request_completion_t & cc) {
+					ninjam_user_list_get = cc.body;
+					get_status = cc.status;
+					if((ninjam_user_list_get.length() > 3) && (get_status ==200)) {
+						if(ninjam_user_list.compare(ninjam_user_list_get) != 0) {
+							ninjam_user_list = ninjam_user_list_get;
+							server_needs_announcement = true;
+						}
+						get_status = 0;
+					}
+					if(server_needs_announcement) {
+						if(ninjam_user_list.compare("No users") == 0) {
+							if(allow_announcements) {
+								bot.message_create(dpp::message(discord_channel , discord_notification_role + ", Server empty. http://ninjam.synthseeker.online/recordings.php" ));
+							}
+							bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_custom, "Server empty."));
+						} else {
+							if(allow_announcements) {
+								bot.message_create(dpp::message(discord_channel , discord_notification_role + ", " + ninjam_user_list + " online! http://ninjam.synthseeker.online/live/" ));
+							}
+							bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_custom, ninjam_user_list + " online!"));
+						}
+						server_needs_announcement = false;
+					}
+				},
+				"",
+				"application/json",
+				{
+					{"Authorization", "Bearer tokengoeshere"}
+				}
+			);
+        }, 15);
     });
  
-    /* Start bot */
     bot.start(dpp::st_wait);
- 
-    return 0;
 }

7274b316120ceb28fbda410996ec071d73cc26ea
jeffmhopkins <jeff.m.hopkins@gmail.com>
2024-06-04 16:49:41 -0400

    Update bot.cpp

--- bot.cpp
+++ bot.cpp
@@ -1,27 +1,24 @@
 #include <dpp/dpp.h>
 
-//version v1.3?
-
-long discord_channel = 0000000000000000000;
-long discord_user_role = 0000000000000000000;
-long discord_admin_role = 0000000000000000000;
-long discord_admin_id = 0000000000000000000;
-std::string discord_notification_role = "<@&0000000000000000000>";
-std::string discord_token = "TOKEN";   //Discord token
-std::string ninjam_server_password ="PASSWORD";
+long discord_channel = 0;
+long discord_user_role = 0;
+long discord_admin_role = 0;
+long discord_admin_id = 0;
+std::string discord_notification_role = "";
+std::string discord_token = "";
+std::string ninjam_server_password = "";
 
 //Internal Temporary Server Variables
 bool server_needs_announcement = false;
 bool allow_announcements = true;
+bool just_booted = true;
 std::string ninjam_user_list = "";
 std::string ninjam_user_list_get = "";
 int get_status = 0;
 
 int main() {
 	dpp::cluster bot(discord_token);
-
 	bot.on_log(dpp::utility::cout_logger());
-
 	bot.on_slashcommand([&bot](const dpp::slashcommand_t & event) {
 		bool command_allowed = false;
 		bool admin_allowed = false;
@@ -31,11 +28,11 @@ int main() {
 			command_allowed = true;
 			admin_allowed = true;
 		} else {
-			for (int i=0; i<roles_list.size(); i++) {
+			for (int i=0; i<roles_list.size(); ++i) {
 				if(roles_list[i] == discord_user_role) {
 					command_allowed = true;
 				}
-				if(roles_list[i] == discord_admin_role) {
+				if(roles_list[i] == discord_user_role) {
 					admin_allowed = true;
 					command_allowed = true;
 				}
@@ -45,14 +42,13 @@ int main() {
 			event.reply(dpp::message("You do not have proper roler membership for this command.").set_flags(dpp::m_ephemeral));
 		} else {
 			if (event.command.get_command_name() == "info") {
-				event.reply(dpp::message("The Synthseeker NINJAM Server address is ninjam.synthseeker.online:2049, server password is " + ninjam_server_password).set_flags(dpp::m_ephemeral));
-				
+				event.reply(dpp::message("The Synthseeker NINJAM Server address is ninjam.synthseeker.online, port 2049, password is '" + ninjam_server_password + "', see http://ninjam.synthseeker.online/connect.html").set_flags(dpp::m_ephemeral));
 			}
 			if(!admin_allowed) {
 				event.reply(dpp::message("You do not have proper role membership for this command.").set_flags(dpp::m_ephemeral));
 			} else {
 				if (event.command.get_command_name() == "settings") {
-						std::string announcement_enable = std::get<std::string>(event.get_parameter("announcements"));
+					std::string announcement_enable = std::get<std::string>(event.get_parameter("announcements"));
 					if(announcement_enable == "announce_enable") {
 						event.reply(dpp::message("Channel announcements are now enabled.").set_flags(dpp::m_ephemeral));
 						allow_announcements = true;
@@ -62,15 +58,30 @@ int main() {
 						allow_announcements = false;
 					}
 				}
+				if (event.command.get_command_name() == "restart_server") {
+					event.reply(dpp::message("Attempting to perform server restart...").set_flags(dpp::m_ephemeral));
+					bot.request(
+						"http://ninjam.synthseeker.online/util/discord.php?a=restart", 
+						dpp::m_get, 
+						[&bot](const dpp::http_request_completion_t & cc) {
+							if(cc.status ==200) {
+								bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_custom, "Restarting Server..."));
+							} else {
+								bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_custom, "Failed restart..."));
+							}
+						}, 
+						"",
+						"application/json",
+						{{"Authorization", "Bearer tokengoeshere"}}
+						);
+				}
 			}
 		}
 	});
- 
 	bot.on_ready([&bot](const dpp::ready_t& event) {
 		//if (dpp::run_once<struct clear_bot_commands>()) {
-		//		bot.global_bulk_command_delete();
+		//	bot.global_bulk_command_delete();
 		//}
-		
 		if (dpp::run_once<struct register_bot_commands>()) {
 			bot.global_command_create(dpp::slashcommand("info", "Information about the NINJAM Server", bot.me.id));
 			dpp::slashcommand settingscommand("settings", "change settings of the NINJAM bot.", bot.me.id);
@@ -79,11 +90,14 @@ int main() {
 				.add_choice(dpp::command_option_choice("disable", std::string("announce_disable")))
 			);
 			bot.global_command_create(settingscommand);
+			bot.global_command_create(dpp::slashcommand("restart_server", "admin command to restart the server", bot.me.id));
+			bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_custom, "Server reboot success"));
 		}
-
 		bot.start_timer([&bot](const dpp::timer& timer){
 			bot.request(
-				"http://ninjam.synthseeker.online/util/discord.php?a=users", dpp::m_get, [&bot](const dpp::http_request_completion_t & cc) {
+				"http://ninjam.synthseeker.online/util/discord.php?a=users", 
+				dpp::m_get,
+				[&bot](const dpp::http_request_completion_t & cc) {
 					ninjam_user_list_get = cc.body;
 					get_status = cc.status;
 					if((ninjam_user_list_get.length() > 3) && (get_status ==200)) {
@@ -96,7 +110,9 @@ int main() {
 					if(server_needs_announcement) {
 						if(ninjam_user_list.compare("No users") == 0) {
 							if(allow_announcements) {
-								bot.message_create(dpp::message(discord_channel , discord_notification_role + ", Server empty. http://ninjam.synthseeker.online/recordings/" ));
+								if(!just_booted) {
+									bot.message_create(dpp::message(discord_channel , discord_notification_role + ", Server empty. http://ninjam.synthseeker.online/recordings.php" ));
+								}
 							}
 							bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_custom, "Server empty"));
 						} else {
@@ -106,16 +122,15 @@ int main() {
 							bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_custom, ninjam_user_list + " online!"));
 						}
 						server_needs_announcement = false;
+						just_booted = false;
 					}
 				},
 				"",
 				"application/json",
-				{
-					{"Authorization", "Bearer tokengoeshere"}
-				}
+				{{"Authorization", "Bearer tokengoeshere"}}
 			);
-		}, 15);
+		}, 
+		15);
 	});
- 
 	bot.start(dpp::st_wait);
 }
